75d7dc8906a626cd33f881d2f226f221d5a53bd0
stumc <stumc@WILBER>
2025-11-09 16:44:25 +0000

    wip

--- src/main.tsx
+++ src/main.tsx
@@ -39,20 +39,3 @@ msalInstance.initialize().then(() => {
   );
 
 });
-
-// Default to using the first account if no account is active on page load
-// if (!msalInstance.getActiveAccount() && msalInstance.getAllAccounts().length > 0) {
-//     // Account selection logic is app dependent. Adjust as needed for different use cases.
-//     msalInstance.setActiveAccount(msalInstance.getAllAccounts()[0]);
-// }
-
-// Listen for sign-in event and set active account
-// msalInstance.addEventCallback((event:any) => {
-//     if (event.eventType === EventType.LOGIN_SUCCESS && event.payload.account) {
-//         const account = event.payload.account;
-//         msalInstance.setActiveAccount(account);
-
-
-//     }
-// });
-
